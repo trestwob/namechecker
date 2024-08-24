1528b9e91403b299bea7460cdb767c38a7ae77c1
trestwob <nilanjanmridha89@gmail.com>
2024-08-24 16:17:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from namechecker.namechecker import Namechecker
+
+def main():
+    obj = Namechecker("Nilanjan")
+    obj.printSpecs()
+
+if __name__ == "__main__":
+    main()
---
+++ namechecker/namechecker.py
@@ -0,0 +1,16 @@
+from json import load
+from os import getcwd
+
+class Namechecker:
+    def __init__(self, name):
+        self.name = name
+
+    
+    def printSpecs(self):
+        path = f"{getcwd()}/namechecker/public/values.json"
+        with open(path) as data:
+            values = load(data)
+            data.close()
+            name = self.name
+            for i in name.lower():
+                print(values[i], end=' ')
